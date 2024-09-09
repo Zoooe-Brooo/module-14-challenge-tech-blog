a59884419e0a6f8a0b3782be5588191b5cdc73e4
Zoooe-Brooo <zhongwenli.zoe@gmail.com>
2024-09-09 21:30:57 +0930

    Update handlebars and login.js

--- public/js/login.js
+++ public/js/login.js
@@ -11,14 +11,14 @@ const loginFormHandler = async (event) => {
   let password = loginPassword.value.trim();
 
   if (username && password) {
-    const response = await fetch("/api/users/login", {
+    const response = await fetch("/api/user/login", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-      document.location.replace("/user/:id");
+      document.location.replace("/user/response.user_id");
     } else {
       alert("Failed to log in.");
     }
